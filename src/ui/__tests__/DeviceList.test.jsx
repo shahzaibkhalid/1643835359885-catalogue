1322eb668fb4a64ac9699686a4a6bb46575f4f2d
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2022-02-03 12:14:36 -0500

    test: :white_check_mark: cover `DeviceList`

---
+++ src/ui/__tests__/DeviceList.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import DeviceList from '../containers/DeviceList';
+import { routes } from '../routes';
+
+/**
+ * Check `src/ui/__test-utils__/mocks/handlers.js` for server mocks
+ */
+
+describe('<DeviceList />',() => {
+  it('should render as many DeviceCard as many sent by the mock server', async () => {
+    render(<DeviceList type={routes.IPHONES} />);
+    expect(await screen.findAllByRole('heading', {level: 4})).toHaveLength(2);
+  });
+
+  it('should render no DeviceCard when mock server sends null', async () => {
+    render(<DeviceList type={routes.WATCHES} />);
+    expect(await screen.findByTestId('device-card-list')).toBeEmptyDOMElement();
+  });
+});
