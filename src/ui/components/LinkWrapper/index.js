48a74745c2896135fec35607465fe45f64302763
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2022-02-03 07:02:03 -0500

    feat(routes): :truck: add `LinkWrapper` to link to `pages`

---
+++ src/ui/components/LinkWrapper/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link as ReactRouterLink } from 'react-router-dom';
+import TdsLink from '@tds/core-link';
+
+const LinkWrapper = ({ children, ...rest }) => (
+  <TdsLink {...rest} reactRouterLinkComponent={rest.to ? ReactRouterLink : undefined}>
+    {children}
+  </TdsLink>
+);
+
+export default LinkWrapper;
