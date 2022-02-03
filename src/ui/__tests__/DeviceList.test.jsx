b4ac37c50bfc463c97c7623f5e819e048b9d5ad9
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2022-02-03 13:25:42 -0500

    chore(deviceList): :white_check_mark: updates to `DeviceList`

--- src/ui/__tests__/DeviceList.test.jsx
+++ src/ui/__tests__/DeviceList.test.jsx
@@ -8,13 +8,13 @@ import { routes } from '../routes';
  */
 
 describe('<DeviceList />',() => {
+  it('should render no DeviceCard when mock server sends null', async () => {
+    render(<DeviceList type={routes.WATCHES} />);
+    expect(await screen.queryAllByRole('heading', {level: 4})).not.toHaveLength(2);
+  });
+
   it('should render as many DeviceCard as many sent by the mock server', async () => {
     render(<DeviceList type={routes.IPHONES} />);
     expect(await screen.findAllByRole('heading', {level: 4})).toHaveLength(2);
-  });
-
-  it('should render no DeviceCard when mock server sends null', async () => {
-    render(<DeviceList type={routes.WATCHES} />);
-    expect(await screen.findByTestId('device-card-list')).toBeEmptyDOMElement();
   });
 });
--- src/ui/containers/DeviceList/index.js
+++ src/ui/containers/DeviceList/index.js
@@ -1,30 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import FlexGrid from '@tds/core-flex-grid';
 import DeviceCard from '../../components/DeviceCard';
-import { getDevicesByType } from '../../api';
-import { getDeviceImgByType } from '../../misc';
+import { getDeviceImgByType, SERVER_URL } from '../../misc';
+import useFetch from '../../hooks/useFetch';
 
 const DeviceList = ({
   type
 }) => {
-  const [devices, setDevices] = useState([]);
-  useEffect(() => {
-    const abortController = new AbortController();
-    const fetchDeviceData = async () => {
-      try {
-        setDevices(await getDevicesByType(type, abortController));
-      } catch(err) {
-        throw new Error(err);
-      }
-    }
-    fetchDeviceData();
-    return () => abortController.abort();
-  }, []);
+  const { response, error } = useFetch(SERVER_URL + type);
+  if (!response?.data) {
+    return null;
+  }
+  if (error) {
+    throw new Error(error);
+  }
   return (
     <>
       <FlexGrid>
         <FlexGrid.Row data-testid='device-card-list'>
-          {devices?.map(device => (
+          {response?.data?.map(device => (
             <FlexGrid.Col
               xs={12}
               sm={6}
