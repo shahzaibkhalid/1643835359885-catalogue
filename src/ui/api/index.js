6225846dff51803b29e28e3517881f6f908a8bba
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2022-02-03 08:02:28 -0500

    refactor(api): :recycle: enhancements to `api` call

--- src/ui/api/index.js
+++ src/ui/api/index.js
@@ -1,13 +1,11 @@
-
 const getDevicesByType = async (type) => {
-  const response = await fetch(`http://localhost:8081${type}`);
-
-  if (!response.ok) {
-    const message = `An error has occured: ${response.status}`;
-    throw new Error(message);
+  try {
+    const response = await fetch(`http://localhost:8081${type}`);
+    const devices = await response.json();
+    return devices?.data ?? [];
+  } catch (err) {
+    throw new Error(err);
   }
-  const devices = await response.json();
-  return devices?.data ?? [];
 }
 
 export { getDevicesByType };
